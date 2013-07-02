3728f67b1bc752ca1b97fa93e475b2df59888a72
mharrys <mattias.harrysson@gmail.com>
2013-07-02 03:27:47 +0200

    Remove custom query class for each model

--- app/models.py
+++ app/models.py
@@ -1,34 +1,11 @@
 from datetime import datetime
 from flask.ext.login import UserMixin
-from flask.ext.sqlalchemy import BaseQuery
-from sqlalchemy import and_, func
 
 from app import db, bcrypt
 from app.helpers import slugify
 
 
-class UserQuery(BaseQuery):
-
-    def filter_by_name(self, name):
-        """Return user with specified name."""
-        return self.filter_by(name=name).first()
-
-    def filter_by_name_or_404(self, name):
-        """Return user with specified name or 404."""
-        return self.filter_by(name=name).first_or_404()
-
-    def filter_by_latest(self):
-        """Return users ordered by latest first."""
-        return self.order_by(User.registered.desc())
-
-    def filter_by_oldest(self):
-        """Return users ordered by oldest first."""
-        return self.order_by(User.registered)
-
-
 class User(db.Model, UserMixin):
-    query_class = UserQuery
-
     id = db.Column(db.Integer, primary_key=True)
     registered = db.Column(db.DateTime, default=datetime.utcnow)
     name = db.Column(db.String(50), nullable=False, unique=True)
@@ -66,55 +43,7 @@ class User(db.Model, UserMixin):
         }
 
 
-class PostQuery(BaseQuery):
-
-    def slug(self, slug, show_hidden=False):
-        """Return post with specified slug."""
-        query = self.filter_by(slug=slug)
-        if not show_hidden:
-            query = query.filter_by(visible=True)
-        return query.first()
-
-    def slug_or_404(self, slug, show_hidden=False):
-        """Return post with specified slug and visibility or 404."""
-        query = self.filter_by(slug=slug)
-        if not show_hidden:
-            query = query.filter_by(visible=True)
-        return query.first_or_404()
-
-    def filter_by_latest(self, show_hidden=False):
-        """Return posts ordered by latest first."""
-        query = self.order_by(Post.published.desc())
-        if not show_hidden:
-            query = query.filter_by(visible=True)
-        return query
-
-    def filter_by_oldest(self, show_hidden=False):
-        """Return posts ordered by oldest first."""
-        query = self.order_by(Post.published)
-        if not show_hidden:
-            query = query.filter_by(visible=True)
-        return query
-
-    def filter_by_title_today(self, title):
-        """Return post with slug generated from current time and title."""
-        slug = slugify(datetime.utcnow(), title)
-        return self.slug(slug)
-
-    def filter_by_date(self, year=None, month=None, day=None):
-        """Return posts by specified date."""
-        terms = []
-        if year:
-            terms.append(func.extract('year', Post.published) == year)
-        if month:
-            terms.append(func.extract('month', Post.published) == month)
-        if day:
-            terms.append(func.extract('day', Post.published) == day)
-        return Post.query.filter(and_(*terms))
-
-
 class Post(db.Model):
-    query_class = PostQuery
     PER_PAGE = 5
 
     id = db.Column(db.Integer, primary_key=True)
@@ -177,28 +106,7 @@ class Post(db.Model):
         }
 
 
-class CommentQuery(BaseQuery):
-
-    def filter_by_name(self, name):
-        """Return comments made by a specific name."""
-        return self.filter_by(name=name).first()
-
-    def filter_by_ip(self, ip):
-        """Return comments made by a specific ip."""
-        return self.filter_by(ip=ip).first()
-
-    def filter_by_latest(self):
-        """Return comments ordered by latest first."""
-        return self.order_by(Comment.posted.desc())
-
-    def filter_by_oldest(self):
-        """Return comments ordered by oldest first."""
-        return self.order_by(Comment.posted)
-
-
 class Comment(db.Model):
-    query_class = CommentQuery
-
     id = db.Column(db.Integer, primary_key=True)
     posted = db.Column(db.DateTime, default=datetime.utcnow)
     name = db.Column(db.String(50), nullable=False)
