849f2cf09c412c0a09865a7edaac9aa739c2a368
Mattias Harrysson <mattias@trixz.se>
2013-08-17 20:18:54 +0200

    add postgresql settings for production server

--- config.py
+++ config.py
@@ -3,5 +3,12 @@ import os
 DEBUG = True
 SECRET_KEY = 'dev'
 
-root = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(root, 'sqlite3.db')
+if DEBUG:
+    root = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(root, 'sqlite3.db')
+else:
+    db = 'database'
+    username = 'username'
+    password = 'password'
+    uri = 'postgres://%s:%s@localhost:5432/%s' % (username, password, db)
+    SQLALCHEMY_DATABASE_URI = uri
